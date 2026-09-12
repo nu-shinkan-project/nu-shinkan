dbbf2ad3f6a869685f8018eec318a3d79d1bd10d
hbenpitsu <hbenpitsu15@gmail.com>
2026-09-12 16:41:59 +0000

    Workflowのlint設定に不足があったので追加

--- .github/eslint.config.js
+++ .github/eslint.config.js
@@ -1,11 +1,21 @@
 import js from "@eslint/js";
+import globals from "globals";
 import { defineConfig } from "eslint/config";
 
 export default defineConfig([
   {
     files: ["**/*.{js,mjs,ts}"],
     ignores: ["node_modules/**", "bin/**"],
-    extends: [js.configs.recommended],
+    extends: [
+      js.configs.recommended,
+      {
+        languageOptions: {
+          globals: {
+            ...globals.node,
+          },
+        },
+      },
+    ],
     rules: {
       "no-unused-vars": [
         "error",
