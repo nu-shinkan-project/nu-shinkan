0940bf4bfa6b4b41e4242faf9989529861c583c8
hbenpitsu <hbenpitsu15@gmail.com>
2026-09-23 13:55:02 +0000

    dummy packageを削除

--- apps/dummy-preview-api/src/interface.ts
+++
@@ -1,7 +0,0 @@
-export const DUMMY_PREVIEW_API_WORKER = "dummy-preview-api";
-export const DUMMY_PREVIEW_API_BINDING = "DUMMY_PREVIEW_API";
-
-export type DummyPreviewMessage = {
-  worker: string;
-  message: string;
-};
